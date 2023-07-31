aec1eaac4e1449105e263285ed65c45a461ade74
francesca <francipeco36@gmail.com>
2023-07-31 16:15:56 +0200

    Automobile class made

--- src/lessons/java/flotta/Automobile.java
+++ src/lessons/java/flotta/Automobile.java
@@ -1,7 +1,24 @@
 package lessons.java.flotta;
 
-public class Automobile {
+public class Automobile extends Veicolo{
     /*
-    Le automobili hanno un campo per indicare il numero di porte, mentre .
+    Le automobili hanno un campo per indicare il numero di porte
      */
+
+    // ATTRIBUTI
+    private int numeroPorte;
+
+    // COSTRUTTORI
+        public Automobile(int numeroTarga, int annoImmatricolazione, int numeroPorte) {
+        super(numeroTarga, annoImmatricolazione);
+        this.numeroPorte=numeroPorte;
+    }
+
+    // GETTER E SETTER
+
+    public int getNumeroPorte() {
+        return numeroPorte;
+    }
+
+    // METODI
 }
