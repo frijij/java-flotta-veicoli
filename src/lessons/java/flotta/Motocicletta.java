812d38bb492096639c47db51e2cdf8e712f5d314
francesca <francipeco36@gmail.com>
2023-07-31 16:22:21 +0200

    Motocicletta class made

--- src/lessons/java/flotta/Motocicletta.java
+++ src/lessons/java/flotta/Motocicletta.java
@@ -2,6 +2,21 @@ package lessons.java.flotta;
 /*
 le motociclette hanno un campo per indicare se sono dotate di cavalletto o meno
  */
-public class Motocicletta {
+public class Motocicletta extends Veicolo{
+    // ATTRIBUTI
+    private boolean conCavalletto;
+
+    // COSTRUTTORI
+    public Motocicletta(int numeroTarga, int annoImmatricolazione, boolean conCavalletto){
+        super(numeroTarga,annoImmatricolazione);
+        this.conCavalletto=conCavalletto;
+    }
+    // GETTER E SETTER
+
+    public boolean isConCavalletto() {
+        return conCavalletto;
+    }
+
+    // METODI
 
 }
