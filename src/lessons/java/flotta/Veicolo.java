346b812a4aa0b3d3f0fa586e00d694212700e7a7
francesca <francipeco36@gmail.com>
2023-07-31 16:13:27 +0200

    Veicolo class made

--- src/lessons/java/flotta/Veicolo.java
+++ src/lessons/java/flotta/Veicolo.java
@@ -5,4 +5,28 @@ public class Veicolo {
     Ogni veicolo ha un numero di targa univoco e un anno di immatricolazione.
     Deve essere possibile visualizzare tutte le informazioni di ciascun tipo di veicolo.
      */
+
+    // ATTRIBUTI
+    private int numeroTarga;
+    private int annoImmatricolazione;
+
+    // COSTRUTTORI
+
+    public Veicolo(int numeroTarga, int annoImmatricolazione) {
+        this.numeroTarga = numeroTarga;
+        this.annoImmatricolazione = annoImmatricolazione;
+    }
+
+
+    // GETTER E SETTER
+
+    public int getNumeroTarga() {
+        return numeroTarga;
+    }
+
+    public int getAnnoImmatricolazione() {
+        return annoImmatricolazione;
+    }
+
+    // METODI
 }
